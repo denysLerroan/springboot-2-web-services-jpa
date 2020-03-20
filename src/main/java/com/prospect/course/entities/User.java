8910df7107a43b9d98eb2fe597ce7f8e69f64045
DenysFerreira <lerroandeco@gmail.com>
2020-03-19 22:41:12 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/prospect/course/entities/User.java
+++ src/main/java/com/prospect/course/entities/User.java
@@ -1,17 +1,21 @@
 package com.prospect.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
-public class User implements Serializable{
+@Table(name = "tb_user")
+public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -19,7 +23,10 @@ public class User implements Serializable{
 	private String email;
 	private String phone;
 	private String password;
-	
+
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+
 	public User() {
 	}
 
@@ -72,6 +79,10 @@ public class User implements Serializable{
 		this.password = password;
 	}
 
+	public List<Order> getOrders() {
+		return orders;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -96,6 +107,5 @@ public class User implements Serializable{
 			return false;
 		return true;
 	}
-	
-	
+
 }
