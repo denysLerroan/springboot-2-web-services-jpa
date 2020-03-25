2527259bc72de0c9737fc7f640e3c241d30af950
DenysFerreira <lerroandeco@gmail.com>
2020-03-25 19:27:12 -0300

    Payment, one-to-one association

--- src/main/java/com/prospect/course/entities/Order.java
+++ src/main/java/com/prospect/course/entities/Order.java
@@ -5,6 +5,7 @@ import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,6 +13,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.Fetch;
@@ -41,6 +43,9 @@ public class Order implements Serializable{
 	@OneToMany(mappedBy = "id.order") 
 	@Fetch(FetchMode.JOIN)
 	private Set<OrderItem> items = new HashSet<>();
+	
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	private Payment payment;
 		
 	public Order() {
 	}
@@ -88,6 +93,14 @@ public class Order implements Serializable{
 		this.client = client;
 	}
 	
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+
 	public Set<OrderItem> getItems(){
 		return items;
 	}
