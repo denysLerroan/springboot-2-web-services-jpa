7eb3ffbaf23c6bb38efed4834a1a09cbe2da48ed
DenysFerreira <lerroandeco@gmail.com>
2020-04-21 02:23:00 -0300

    Exception handling Category

--- src/main/java/com/prospect/course/services/CategoryService.java
+++ src/main/java/com/prospect/course/services/CategoryService.java
@@ -3,11 +3,17 @@ package com.prospect.course.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.prospect.course.entities.Category;
 import com.prospect.course.repositories.CategoryRepository;
+import com.prospect.course.services.exceptions.DataBaseException;
+import com.prospect.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class CategoryService {
@@ -21,7 +27,7 @@ public class CategoryService {
 	
 	public Category findById(Long id) {
 		Optional<Category> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public Category insert(Category obj) {
@@ -29,13 +35,23 @@ public class CategoryService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		}catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DataBaseException(e.getMessage());
+		}
 	}
 	
 	public Category update(Long id, Category obj) {
+		try {
 			Category entity = repository.getOne(id);
 			updateDate(entity, obj);
 			return repository.save(entity);
+		}catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateDate(Category entity, Category obj) {
