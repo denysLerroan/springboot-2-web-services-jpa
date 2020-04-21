8b99f3bc2f289a248ad384a999be15bfe6da55b1
DenysFerreira <lerroandeco@gmail.com>
2020-04-21 01:55:44 -0300

    Category insert

--- src/main/java/com/prospect/course/services/CategoryService.java
+++ src/main/java/com/prospect/course/services/CategoryService.java
@@ -23,5 +23,9 @@ public class CategoryService {
 		Optional<Category> obj = repository.findById(id);
 		return obj.get();
 	}
+	
+	public Category insert(Category obj) {
+		return repository.save(obj);
+	}
 
 }
